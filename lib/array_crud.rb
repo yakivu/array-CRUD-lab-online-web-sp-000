7bf994a89c988403ccb437a90c6a141ccc677f7e
Adam Walker <adamw207@yahoo.com>
2020-02-20 15:56:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,10 @@ end
 
 def add_element_to_end_of_array(array, element)
   [array,element]
+  << ""
 end
 
-def add_element_to_start_of_array(array, element).first("wow")
+def add_element_to_start_of_array(array, element)
   [array,element]
 end
 
